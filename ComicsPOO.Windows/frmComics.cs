4bb8b5a037d12d049e02c66ea9f895d0911ded34
ciminoce <ciminoce@yahoo.com.ar>
2024-12-05 16:54:33 -0300

    Agregar archivos de proyecto.

---
+++ ComicsPOO.Windows/frmComics.cs
@@ -0,0 +1,13 @@
+using ComicsPOO.Windows.Helpers;
+
+namespace ComicsPOO.Windows
+{
+    public partial class frmComics : Form
+    {
+        public frmComics()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
